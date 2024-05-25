656751abdd78867e179d630965a80c432d525653
Avish Jha <avish.j@protonmail.com>
2024-05-26 00:31:13 +0530

    initialise projects
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

---
+++ Java/src/main/java/dev/avish/projects/App.java
@@ -0,0 +1,13 @@
+package dev.avish.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
