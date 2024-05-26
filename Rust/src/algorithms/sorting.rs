3415732b0748dd1bfe6e6bff1c5994cb2155d94d
Avish Jha <avish.j@protonmail.com>
2024-05-27 01:10:56 +0530

    feat: insertion sort
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- Rust/src/algorithms/sorting.rs
+++ Rust/src/algorithms/sorting.rs
@@ -1,4 +1,6 @@
-pub struct Sorting<T: PartialOrd> {
+use std::fmt::Debug;
+
+pub struct Sorting<T: PartialOrd + Clone> {
     pub array: Vec<T>,
 }
 
@@ -16,7 +18,7 @@ impl Order {
     }
 }
 
-impl<T: PartialOrd> Sorting<T> {
+impl<T: PartialOrd + Clone> Sorting<T> {
     pub fn selection(&mut self, order: Order) {
         for i in 0..self.array.len() {
             let mut min_idx = i;
@@ -42,6 +44,21 @@ impl<T: PartialOrd> Sorting<T> {
             if !had_swap_in_pass {
                 break;
             }
+        }
+    }
+
+    pub fn insertion(&mut self, order: Order) {
+        for i in 1..self.array.len() {
+            let k = self.array[i].clone();
+            let mut j = i - 1;
+            while order.comparator(&self.array[i], &self.array[j]) {
+                self.array[j + 1] = self.array[j].clone();
+                if j == 0 {
+                    break;
+                }
+                j -= 1;
+            }
+            self.array[j + 1] = k;
         }
     }
 }
