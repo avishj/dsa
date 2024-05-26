549596c97265aaebf035cc5da421886afa384453
Avish Jha <avish.j@protonmail.com>
2024-05-27 00:54:53 +0530

    feat: bubble sort + add comparator for ordering
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- Rust/src/algorithms/sorting.rs
+++ Rust/src/algorithms/sorting.rs
@@ -7,25 +7,41 @@ pub enum Order {
     Descending,
 }
 
+impl Order {
+    pub fn comparator<T: PartialOrd>(&self, a: &T, b: &T) -> bool {
+        match self {
+            Order::Ascending => a < b,
+            Order::Descending => a > b,
+        }
+    }
+}
+
 impl<T: PartialOrd> Sorting<T> {
     pub fn selection(&mut self, order: Order) {
         for i in 0..self.array.len() {
             let mut min_idx = i;
             for j in i + 1..self.array.len() {
-                match order {
-                    Order::Ascending => {
-                        if self.array[j] < self.array[j + 1] {
-                            min_idx = j;
-                        }
-                    }
-                    Order::Descending => {
-                        if self.array[j] > self.array[j + 1] {
-                            min_idx = j;
-                        }
-                    }
+                if order.comparator(&self.array[j], &self.array[j + 1]) {
+                    min_idx = j;
                 }
             }
             self.array.swap(i, min_idx);
+        }
+    }
+
+    pub fn bubble(&mut self, order: Order) {
+        let mut had_swap_in_pass;
+        for i in 0..self.array.len() - 1 {
+            had_swap_in_pass = false;
+            for j in 0..self.array.len() - i - 1 {
+                if order.comparator(&self.array[j], &self.array[j + 1]) {
+                    self.array.swap(j, j + 1);
+                    had_swap_in_pass = true;
+                }
+            }
+            if !had_swap_in_pass {
+                break;
+            }
         }
     }
 }
